36d9e7e48e5c049d3a0b72cca2aebe1fcca3748e
Mark <m@rkw.me>
2014-05-08 07:11:49 +0000

    fixed mistake

--- migrations/m140507_191043_nullable_foreign_keys.php
+++ migrations/m140507_191043_nullable_foreign_keys.php
@@ -10,12 +10,6 @@ class m140507_191043_nullable_foreign_keys extends CDbMigration
 		$this->alterColumn('et_ophcipatientadmission_npostatus','site_marked_by_id','int(10) unsigned null');
 		$this->alterColumn('et_ophcipatientadmission_npostatus','time_last_ate','datetime null');
 		$this->alterColumn('et_ophcipatientadmission_npostatus','time_last_drank','datetime null');
-		$this->alterColumn('et_ophcipatientdischarge_discharge','translator_present_id','int(10) unsigned null');
-		$this->alterColumn('et_ophcipatientdischarge_discharge','patient_handoff_to_id','int(10) unsigned null');
-		$this->alterColumn('et_ophcipatientdischarge_discharge','patient_emergency_contact_id','int(10) unsigned null');
-		$this->alterColumn('et_ophcipatientdischarge_discharge','patient_followup_contact_id','int(10) unsigned null');
-		$this->alterColumn('et_ophcipatientdischarge_discharge','surgical_case_review_contact_id','int(10) unsigned null');
-		$this->alterColumn('et_ophcipatientdischarge_discharge','nurse_ophthalmologist_id','int(10) unsigned null');
 	}
 
 	public function down()
@@ -26,11 +20,5 @@ class m140507_191043_nullable_foreign_keys extends CDbMigration
     $this->alterColumn('et_ophcipatientadmission_npostatus','site_marked_by_id','int(10) unsigned not null');
 		$this->alterColumn('et_ophcipatientadmission_npostatus','time_last_ate','datetime not null');
 		$this->alterColumn('et_ophcipatientadmission_npostatus','time_last_drank','datetime not null');
-    $this->alterColumn('et_ophcipatientdischarge_discharge','translator_present_id','int(10) unsigned not null');
-    $this->alterColumn('et_ophcipatientdischarge_discharge','patient_handoff_to_id','int(10) unsigned not null');
-    $this->alterColumn('et_ophcipatientdischarge_discharge','patient_emergency_contact_id','int(10) unsigned not null');
-    $this->alterColumn('et_ophcipatientdischarge_discharge','patient_followup_contact_id','int(10) unsigned not null');
-    $this->alterColumn('et_ophcipatientdischarge_discharge','surgical_case_review_contact_id','int(10) unsigned not null');
-    $this->alterColumn('et_ophcipatientdischarge_discharge','nurse_ophthalmologist_id','int(10) unsigned not null');
 	}
 }
